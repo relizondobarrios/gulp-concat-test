2d175207614c2cf3d99dfc4c5878990b4b47e532
Raquel Elizondo <rackel_elizondo@premierway.com>
2015-10-16 11:04:28 -0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+/*
+* Dependencias
+*/
+var gulp = require('gulp'),
+  jshint = require('gulp-jshint'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify');
+
+/*
+* Configuración de la tarea 'demo'
+*/
+gulp.task('demo', function () {
+  gulp.src('js/source/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('todo.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js/build/'))
+});
